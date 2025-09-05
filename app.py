11c9660e43f966fb8b2028c1166aa2101b462d0c
Shivi Gupta <shivigupta3113@gmail.com>
2025-09-06 02:03:55 +0530

    project complete

---
+++ app.py
@@ -0,0 +1,93 @@
+from flask import Flask, render_template, jsonify, request
+from src.helper import download_hugging_face_embeddings
+from langchain_pinecone import PineconeVectorStore
+from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI 
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from dotenv import load_dotenv
+from src.prompt import *
+import os
+
+
+app = Flask(__name__)
+
+
+load_dotenv()
+
+PINECONE_API_KEY=os.environ.get('PINECONE_API_KEY')
+OPENAI_API_KEY=os.environ.get('OPENAI_API_KEY')
+GEMINI_API_KEY=os.environ.get('GEMINI_API_KEY')
+
+os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+os.environ["GEMINI_API_KEY"] = GEMINI_API_KEY
+
+
+embeddings = download_hugging_face_embeddings()
+
+index_name = "medical-chatbot" 
+# Embed each chunk and upsert the embeddings into your Pinecone index.
+docsearch = PineconeVectorStore.from_existing_index(
+    index_name=index_name,
+    embedding=embeddings
+)
+
+
+
+
+retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k":3})
+chatModel = ChatGoogleGenerativeAI(
+    model="gemini-1.5-flash",     # You can also try "gemini-1.5-flash" or "gemini-1.5-pro"
+    google_api_key=GEMINI_API_KEY,
+    temperature=0.2
+)
+
+# chatModel = ChatOpenAI(model="gpt-4o")
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        ("human", "{input}"),
+    ]
+)
+
+question_answer_chain = create_stuff_documents_chain(chatModel, prompt)
+rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+
+memory = ConversationBufferMemory(
+    memory_key="chat_history",
+    return_messages=True
+)
+
+# ✅ Conversational RAG chain (works with memory)
+rag_chain_with_memory = ConversationalRetrievalChain.from_llm(
+    llm=chatModel,
+    retriever=retriever,
+    memory=memory,
+    combine_docs_chain_kwargs={"prompt": ChatPromptTemplate.from_messages(
+        [("system", system_prompt), ("human", "{question}")]
+    )}
+)
+
+@app.route("/")
+def index():
+    return render_template('chat.html')
+
+
+
+@app.route("/get", methods=["GET", "POST"])
+def chat():
+    msg = request.form["msg"]
+    input = msg
+    print(input)
+    response = rag_chain.invoke({"input": msg})
+    print("Response : ", response["answer"])
+    return str(response["answer"])
+
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port= 8080, debug= True)
